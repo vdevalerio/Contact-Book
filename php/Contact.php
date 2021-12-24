53829a76d34062f04f7868a682a759e1d550c676
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-24 10:12:57 -0300

    Add methods set e get

--- php/Contact.php
+++ php/Contact.php
@@ -3,6 +3,23 @@
 	{
 		private $name;
 		private $phone;
+
+		function set_name($name)
+		{
+			$this->name = $name;
+		}
+		function get_name()
+		{
+			return $this->name;
+		}
+		function set_phone($phone)
+		{
+			$this->phone = $phone;
+		}
+		function get_phone()
+		{
+			return $this->phone;
+		}
 	}
 
 # TODO:

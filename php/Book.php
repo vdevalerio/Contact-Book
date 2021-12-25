e579237199145cc85259ece9ade76928d60a9507
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-25 15:52:14 -0300

    Add printContatList() method and uncomment tests

--- php/Book.php
+++ php/Book.php
@@ -16,6 +16,15 @@
 		{
 			return $this->contactList;
 		}
+		public function printContactList()
+		{
+			foreach($this->getContactList() as $element)
+			{
+				echo "Name: " .$element->getName(). "<br/>";
+				echo "Phone: " .$element->getPhone(). "<br/>";
+				echo "<br/>";
+			}
+		}
 		public function deleteContact($name)
 		{
 			for($i = 0; $i < count($this->contactList); $i++)
@@ -39,28 +48,18 @@
 			}
 		}
 	}
-	#$book = new Book();
-	#$book->createContact("Name 1", "Phone 1");
-	#$book->createContact("Name 2", "Phone 2");
-	#$book->createContact("Name 3", "Phone 3");
-	#$book->createContact("Name 4", "Phone 4");
-	#$book->createContact("Name 5", "Phone 5");
+	$book = new Book();
+	$book->createContact("Name 1", "Phone 1");
+	$book->createContact("Name 2", "Phone 2");
+	$book->createContact("Name 3", "Phone 3");
+	$book->createContact("Name 4", "Phone 4");
+	$book->createContact("Name 5", "Phone 5");
 
-	#echo "Before editing:<br/>";
-	#foreach($book->getContactList() as $element)
-	#{
-	#	echo "Name: " .$element->getName(). "<br/>";
-	#	echo "Phone: " .$element->getPhone(). "<br/>";
-	#	echo "<br/>";
-	#}
+	echo "Before editing:<br/>";
+	$book->printContactList();
 
-	#$book->editContact("Name 3", "Boladaum", "123123");
+	$book->editContact("Name 3", "Boladaum", "123123");
 
-	#echo "After editing:<br/>";
-	#foreach($book->getContactList() as $element)
-	#{
-	#	echo "Name: " .$element->getName(). "<br/>";
-	#	echo "Phone: " .$element->getPhone(). "<br/>";
-	#	echo "<br/>";
-	#}
+	echo "After editing:<br/>";
+	$book->printContactList();
 ?>

b95dbfa46eb05fb7150d75f5e244713f92439107
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-25 18:23:10 -0300

    Remove printing functionalities from Book class and the comments too

--- php/Book.php
+++ php/Book.php
@@ -16,14 +16,6 @@
 		{
 			return $this->contactList;
 		}
-		public function printContactList()
-		{
-			foreach($this->getContactList() as $element)
-			{
-				echo "Name: " .$element->getName(). "<br/>";
-				echo "Phone: " .$element->getPhone(). "<br/>";
-			}
-		}
 		public function deleteContact($name)
 		{
 			for($i = 0; $i < count($this->contactList); $i++)
@@ -47,18 +39,4 @@
 			}
 		}
 	}
-	#$book = new Book();
-	#$book->createContact("Name 1", "Phone 1");
-	#$book->createContact("Name 2", "Phone 2");
-	#$book->createContact("Name 3", "Phone 3");
-	#$book->createContact("Name 4", "Phone 4");
-	#$book->createContact("Name 5", "Phone 5");
-
-	#echo "Before editing:<br/>";
-	#$book->printContactList();
-
-	#$book->editContact("Name 3", "Boladaum", "123123");
-
-	#echo "After editing:<br/>";
-	#$book->printContactList();
 ?>

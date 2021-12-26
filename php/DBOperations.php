8f94ebda610230c5f97e74ee138a7ecc2fb9a70d
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-26 01:27:47 -0300

    Add some DB Operations: openConnection(), createDataBase() and closeConnection()

---
+++ php/DBOperations.php
@@ -0,0 +1,32 @@
+<?php
+	function openConnection()
+	{
+		$dbhost = "127.0.0.1";
+		$dbuser = "root";
+		$dbpass = "MQjLTkEoTc714U][";
+
+		$connection = new mysqli($dbhost, $dbuser, $dbpass);
+
+		if ($connection->connect_error)
+		{
+			die("Connection failed: " .$connection->connect_error);
+		}
+		
+		return $connection;
+	}
+	function closeConnection($connection)
+	{
+		$connection->close();
+	}
+	function createDataBase($connection, $name){
+		$sql = "CREATE DATABASE " .$name;
+		if ($connection->query($sql) === TRUE)
+		{
+			echo "Database created successfully";
+		}
+		else
+		{
+			echo "Error creating database: " .$connection->error;
+		}
+	}
+?>

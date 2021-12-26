acbe10be94ca90a3610f52bf21c6f672501209f4
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-26 18:24:40 -0300

    Make variables table's fields

--- php/Book.php
+++ php/Book.php
@@ -9,6 +9,8 @@
 		private $connection;
 		private $dbName = "agenda";
 		private $tableName = "Contact";
+		private $field1 = "name";
+		private $field2 = "phone";
 
 		# Methods
 		public function initializeDataBase()
@@ -30,8 +32,8 @@
 			$this->connection = openConnectionDataBase($this->dbName);
 			$table = " (
 				id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-				name VARCHAR(30) NOT NULL,
-				phone VARCHAR(11) NOT NULL
+				$field1 VARCHAR(30) NOT NULL,
+				$field2 VARCHAR(11) NOT NULL
 			) ";
 			dropTable($this->connection, $this->tableName, $table);
 			dropDataBase($this->connection, $this->dbName);

4d7cb7f8c52c04ca80b6bcf52d77bd634ea7d8b5
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-27 17:25:24 -0300

    Put input recievement in responseContactCreation.php

--- php/responseContactCreation.php
+++ php/responseContactCreation.php
@@ -1,5 +1,12 @@
 <?php
 	echo file_get_contents("../html/header.html");
+	include("IOHandler.php");
+	$handler = new IOHandler();
+	$handler->initBook();
+	if(isset($_POST["name"]) && isset($_POST["phone"]))
+	{
+		$handler->getBook()->createContact($_POST["name"], $_POST["phone"]);
+	}
 	echo file_get_contents("../html/response.html");
 	echo file_get_contents("../html/footer.html");
 ?>

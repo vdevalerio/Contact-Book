f2e1e2a93b140bbabd4f610fa0911a92f037d715
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-27 18:29:09 -0300

    Change initial page to menu.html

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
 	echo file_get_contents("../html/header.html");
-	echo file_get_contents("../html/form.html");
+	echo file_get_contents("../html/menu.html");
 	echo file_get_contents("../html/footer.html");
 ?>

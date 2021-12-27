8e48979a77910ded9728d315b59a3a895cd8d95d
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-27 11:01:02 -0300

    Remove useless echo's

--- php/index.php
+++ php/index.php
@@ -1,11 +1,5 @@
 <?php echo file_get_contents("../html/header.html"); ?>
 <?php
-	echo "<br/>";
-	echo "<br/>";
-	echo "<br/>";
-	echo "<br/>";
-	echo "<br/>";
-	echo "<br/>";
 	echo file_get_contents("../html/form.html"); 
 ?>
 <?php echo file_get_contents("../html/footer.html"); ?>

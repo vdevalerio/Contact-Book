9ec2dd2280539ad48c7e300643d1409553f76919
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-28 20:00:29 -0300

    Add html file in the contactList page

--- php/contactList.php
+++ php/contactList.php
@@ -3,6 +3,8 @@
 	include ("IOHandler.php");
 	$handler = new IOHandler();
 	$handler->initBook();
+	echo file_get_contents("../html/contactListTop.html");
 	$handler->getBook()->getContactList();
+	echo file_get_contents("../html/contactListBottom.html");
 	echo file_get_contents("../html/footer.html");
 ?>

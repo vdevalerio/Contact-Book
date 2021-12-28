be93a53f4aa307dd7b43a980c7ca22124567015b
Marcos Valério <marcosvaleriox12@hotmail.com>
2021-12-28 15:12:00 -0300

    Add respectives behaviors of button input recieve

--- php/responseMenu.php
+++ php/responseMenu.php
@@ -1,28 +1,56 @@
 <?php
-	#echo file_get_contents("../html/header.html");
+	echo file_get_contents("../html/header.html");
 
 	include("Book.php");
 	$book = new Book();
+	$dbExists = $book->checkDBExistence();
 
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['initAgenda']))
 	{
-		echo "INIT DB";
-		$book->initializeDataBase();
-		// Add respective html
+		if(!$dbExists)
+		{
+			$book->initializeDataBase();
+			echo file_get_contents("../html/agendaCreated.html");
+		}
+		else
+		{
+			echo file_get_contents("../html/agendaAlredyExists.html");
+		}
 	}
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['addContact']))
 	{
-		echo "ADD CONTACT";
-		// Add respective html
+		if($dbExists)
+		{
+			echo file_get_contents("../html/addContact.html");
+		}
+		else
+		{
+			echo file_get_contents("../html/thereIsNoAgenda.html");
+		}
 	}
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['searchContact']))
 	{
-		echo "SEARCH CONTACT";
-		// Add respective html
+		if($dbExists)
+		{
+			echo file_get_contents("../html/searchContact.html");
+		}
+		else
+		{
+			echo file_get_contents("../html/thereIsNoAgenda.html");
+		}
 	}
 	if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['delAgenda']))
 	{
-		echo "DEL AGENDA";
-		// Add respective html
+		if($dbExists)
+		{
+			$book->deleteDataBase();
+			echo file_get_contents("../html/agendaDeleted.html");
+		}
+		else
+		{
+			echo file_get_contents("../html/thereIsNoAgenda.html");
+		}
 	}
+
+	echo file_get_contents("../html/footer.html");
 ?>
